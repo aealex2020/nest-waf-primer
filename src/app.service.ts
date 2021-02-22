e87d5832a56591726d4aac109c3a7687e4086ae2
aealex2020 <izebhijie.esekhile@pau.edu.ng>
2021-02-22 11:13:32 +0100

    new changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  @Injectable()
+  export class AppService {
+    getHello(): string {
+      return 'Hello World!';
+    }
+    getHello2(): {} {
+      return {
+        message: 'Hello World!', title: 'My Greeting App'};
+      }
+    getHome(): {} {
+      return { title: 'Home Page' };
+    }
+    getAboutUs(): {} {
+      return { title: 'About Us Page' };
+    }
   }
-}
